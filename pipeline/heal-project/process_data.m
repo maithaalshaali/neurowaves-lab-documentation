30ad2a17a5bd129f68d3f036f1e87269f2dc545d
maithaalshaali <ma6895@nyu.edu>
2025-07-15 12:15:26 +0400

    Add Heal Project data processing Python script

--- pipeline/heal-project/process_data.m
+++
@@ -1,4 +0,0 @@
-function output = process_data(input)
-    % A simple example function for data processing
-    output = input .* 2;
-end
---
+++ pipeline/mne_pipelines/heal-project_resting-state/resting-state_pipeline.py
@@ -0,0 +1,9 @@
+import mne
+import matplotlib.pyplot as plt
+
+data_path = "/Users/ma6895/Library/CloudStorage/Box-Box/heal-project/before_intervention/sub-001/meg/sub-001_heal_eyesopen_eyesclosed-1.con"
+raw_data = mne.io.read_raw_kit(data_path)
+print(raw_data.info.ch_names)
+raw_data.plot()
+plt.show()
+a=1
