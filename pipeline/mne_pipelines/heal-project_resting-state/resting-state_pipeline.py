b674514ab58f901273a9976a00d7e1e35663144d
maithaalshaali <ma6895@nyu.edu>
2025-07-15 12:26:32 +0400

    Add Heal Project data processing Python script

--- pipeline/mne_pipelines/heal-project_resting-state/resting-state_pipeline.py
+++ pipeline/mne_pipelines/heal-project_resting-state/resting-state_pipeline.py
@@ -1,6 +1,8 @@
 import mne
 import matplotlib.pyplot as plt
 
+
+# g hg gh h
 data_path = "/Users/ma6895/Library/CloudStorage/Box-Box/heal-project/before_intervention/sub-001/meg/sub-001_heal_eyesopen_eyesclosed-1.con"
 raw_data = mne.io.read_raw_kit(data_path)
 print(raw_data.info.ch_names)
